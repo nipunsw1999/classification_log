2e7fd535b87444d7df1bed307860ae6a52e4310c
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-08 17:57:00 +0530

    bert updated

--- processor_bert.py
+++ processor_bert.py
@@ -1,17 +1,29 @@
 from sentence_transformers import SentenceTransformer
 import joblib
 
+# Load the saved model
+classifier_model = joblib.load("models/log_classifier.joblib")
+
+# Load the SentenceTransformer model to generate embeddings
+transformer_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def classify_with_bert(log_message):
-    # Load the saved model
-    classifier_model = joblib.load("models/log_classifier.joblib")
-
-    # Load the SentenceTransformer model to generate embeddings
-    transformer_model = SentenceTransformer('all-MiniLM-L6-v2')
-
     # Generate the log message embedding
     message_embedding = transformer_model.encode(log_message)
 
     # Predict the classification label
     predicted_class = classifier_model.predict(message_embedding)[0]
     return predicted_class
+
+
+if __name__ == "__main__":
+    log_message = [
+        "User User1 logged in."
+        "Backup started at 12:00."
+        "Backup completed successfully."
+        "System updated to version 1.0."
+    ]
+
+    for log in log_message:
+        label = classify_with_bert(log)
+        print(log, "->", label)
